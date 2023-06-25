c14c61ba33f41af4f372ee992a4495235eb8c27d
David <dmagill89@gmail.com>
2023-06-25 14:47:34 -0700

    remove files that should not be included in source control and update docs.

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/david/Projects/arduinobot_ws/src
-# Build directory: /home/david/Projects/arduinobot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("arduinobot_description")
-subdirs("arduinobot_test")
